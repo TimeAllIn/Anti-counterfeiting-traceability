e3d30b6a49b3de53efcf3b714bf4f01f684ad74a
TimeAllIn <22404100117@stu.hut.edu.cn>
2026-05-17 09:08:00 +0800

    更新

--- net.gd
+++ net.gd
@@ -7,6 +7,7 @@ func create_net(port:int = 2233):
 		self.multiplayer.multiplayer_peer =  peer
 		print("成功创建服务器")
 		print(self.multiplayer.get_unique_id())
+		数据 = load_json_to_dict("user://server_data.json")
 	
 #客户端连接
 func conect_net(ip:String="127.0.0.1",port:int = 2233):
@@ -18,8 +19,8 @@ func conect_net(ip:String="127.0.0.1",port:int = 2233):
 	await get_tree().create_timer(0.5).timeout
 	数据传输()
 
-#var 数据:Dictionary = {"防伪码Test":{}}
-var 数据
+var 数据:Dictionary = {"防伪码Test":{}}
+#var 数据
 #文件传输
 @rpc("any_peer","call_remote")
 func data_conect(temp_id):
@@ -45,6 +46,7 @@ func 下级_load(公司名称:String,value):
 			return
 		数据[公司名称] = value
 		emit_signal("客户端信息发送")
+		save_dict_to_json("user://server_data.json",数据)
 	pass
 	
 @rpc("any_peer","call_remote")
@@ -67,6 +69,36 @@ func 数据传输():
 func 数据上传(公司名称:String,value):
 	下级_load.rpc_id(1,公司名称,value)
 	pass
+
+func save_dict_to_json(path: String, data: Dictionary) -> void:
+	# 转换为格式化 JSON
+	var json_str = JSON.stringify(data, "\t")
+	# 写入文件
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	if file:
+		file.store_string(json_str)
+		file.close()
+		print("保存成功: " + path)
+	else:
+		print("保存失败")
+		
+func load_json_to_dict(path: String) -> Dictionary:
+	if not FileAccess.file_exists(path):
+		return {}
+	var file = FileAccess.open(path, FileAccess.READ)
+	if file:
+		var json_str = file.get_as_text()
+		file.close()
+		# 解析 JSON
+		var result = JSON.parse_string(json_str)
+		if result is Dictionary:
+			return result
+		else:
+			print("JSON 不是字典")
+			return {"防伪码Test":{}}
+	else:
+		print("读取失败")
+		return {"防伪码Test":{}}
 
 
 func 用户查询(md5:String):
